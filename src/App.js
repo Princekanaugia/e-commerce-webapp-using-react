8ddc19842914d97570f3ea9cd585b09be4bb6c33
Prince Kanaugia <pkanaugia@gmail.com>
2023-08-29 09:56:27 +0530

    initaial backend commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./component/Navbar";
 import Home from "./component/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      <Router>
+        <Navbar title="Shopkart" />
+        <Routes>
+          <Route path="/" element={<Home/>} >
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
